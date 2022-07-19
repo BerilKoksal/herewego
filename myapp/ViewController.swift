671902254f569577a6a16bc478423148de6e2b16
Rashid Ramazanov <rasid.ramazanov@mobven.com>
2022-07-19 20:51:53 +0300

    Set image of imageView in view controller from api.

--- myapp/ViewController.swift
+++ myapp/ViewController.swift
@@ -11,11 +11,25 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var myLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fetchImage()
     }
 
+    private func fetchImage() {
+        let url = URL(string: "https://i1.sndcdn.com/artworks-CYsqCXTXPYrfv6Ki-orhJ5g-t500x500.jpg")
+        let task = URLSession.shared.dataTask(with: url!) { data, response, error in
+            guard let data = data else {
+                return
+            }
+            let image = UIImage(data: data)
+            DispatchQueue.main.async {
+                self.imageView.image = image
+            }
+        }
+        task.resume()
+    }
 
     @IBAction func changeButtonClicked(_ sender: Any) {
         imageView.image = UIImage(named: "magnum")
